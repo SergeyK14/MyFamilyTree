d199c5dd20bfc6524cab489dd5165e49765372fc
SergeyK14 <serega_konchakov@mail.ru>
2023-06-08 19:37:31 +0300

    Creation a class Gender

---
+++ Gender.java
@@ -0,0 +1,23 @@
+package Project_MyFamilyTree;
+
+public enum Gender {
+    man,
+    woman;
+    
+//     public Gender gender;
+    
+//     Gender (Gender gender){
+//         this.gender = gender;
+//     }
+    
+//     public Gender getGender(){
+//         return gender;
+//     }
+
+// @Override
+// public String toString() {
+//     return "gender = " + gender;
+// }
+
+}
+
