86d538f09a6c5015dcd99dbe604062a92ac53884
SergeyK14 <serega_konchakov@mail.ru>
2023-06-15 20:50:40 +0300

    Editing

--- FileHendler.java
+++ FileHendler.java
@@ -2,24 +2,30 @@ package Project_MyFamilyTree;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.util.List;
 
-public class FileHendler implements Serializable{
-    //Human human;
-    //FamilyTree humans;
+public class FileHendler implements Saveable{
+    Human human;
+    FamilyTree humans;
     
     FileHendler fileHendler = new FileHendler();
    
-    public void saveMethod(){
-        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("FamilyTree.out"));
-            out.writeObject(fileHendler);
-            out.close();
+    public void saveMethod() {
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("FamilyTree.out"));
+        objectOutputStream.writeObject(humans);
+        objectOutputStream.writeObject(human_1);
+        objectOutputStream.close();
         }
     public void readMethod(){
-        ObjectInputStream in = new ObjectInputStream(new FileInputStream("FamilyTree.out"));
-        fileHendler = (FileHendler) in.readObject();
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("FamilyTree.out"));
+        FamilyTree humans_Restored = (FamilyTree) objectInputStream.readObject();
+        Human human1_Restored = (Human) objectInputStream.readObject();
+        objectInputStream.close();
     }
 }
+
+
+        
+        
